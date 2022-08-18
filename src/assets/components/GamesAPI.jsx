a9dedf3c8da4a4bd1e36a0b08c65c6e3e84c68b3
Fernando Diaz <70718664+aboueidlautaro@users.noreply.github.com>
2022-08-17 21:17:04 -0300

    base api fetch with validations

--- src/assets/components/GamesAPI.jsx
+++ src/assets/components/GamesAPI.jsx
@@ -1,16 +1,46 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXbox } from "@fortawesome/free-brands-svg-icons";
+import { faDesktop } from "@fortawesome/free-solid-svg-icons";
 
 function GamesAPI({ results = [] }) {
   return (
     <>
-      {results.map((value, index) => (
-        <div key={index}>
-          <div className="text-center">
-            <p>{value.id}</p>
-            <p>{value.name}</p>
+      {results.map((value, index) => {
+        return (
+          <div key={index}>
+            <div className="text-center text-hueso">
+              <h2>{value.name}</h2>
+              <small>{value.released}</small>
+              {value.platforms.map((valor, id) => {
+                {
+                  (() => {
+                    switch (valor.platform.slug) {
+                      case "xbox":
+                        return (
+                          <p>
+                            <FontAwesomeIcon icon={faXbox} />
+                          </p>
+                        );
+                      case "pc":
+                        return (
+                          <p>
+                            <FontAwesomeIcon icon={faDesktop} />
+                          </p>
+                        );
+                    }
+                  })();
+                }
+                return (
+                  <div key={id}>
+                    <span>{valor.platform.slug}</span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 }
